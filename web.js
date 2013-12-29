498359325c847219e0a53ca4eec26980365d4cfa
Yuhan Zhang <david8373@gmail.com>
2013-12-29 21:14:21 +0000

    write to disk

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ var app = express.createServer(express.logger());
 var msg = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
+  fs.writeFileSync(__dirname + '/output.txt', 'Hello Node', function (err) {
+      if (err) throw err;
+      console.log('It\'s saved!');
+  });
   response.send(msg);
 });
 
